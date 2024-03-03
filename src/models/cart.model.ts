b049cbab49ae9071db8b638d7caa353d3384e4da
khangnguyen2100 <khangnguyen23112100@gmail.com>
2024-03-03 16:49:42 +0700

    fix: fix calc product amount in cart

--- src/models/cart.model.ts
+++ src/models/cart.model.ts
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 import { COLLECTION_NAMES, DOCUMENT_NAMES } from 'src/constants/enums/common';
+import { CartProduct } from 'src/constants/types/Cart';
 
 const cartSchema = new mongoose.Schema(
   {
@@ -39,6 +40,7 @@ const cartSchema = new mongoose.Schema(
     collection: COLLECTION_NAMES.CART,
   },
 );
+
 cartSchema.pre('save', async function (next) {
   if (this.isModified('cartProducts')) {
     this.cartCountProducts = this.cartProducts.reduce((arr, curr) => {
@@ -46,6 +48,28 @@ cartSchema.pre('save', async function (next) {
     }, 0);
   }
   next();
+});
+cartSchema.post('findOne', async function (doc, next) {
+  if (doc) {
+    doc.cartCountProducts = doc.cartProducts.reduce(
+      (arr: number, curr: CartProduct) => {
+        return arr + curr.quantity;
+      },
+      0,
+    );
+    next();
+  }
+});
+cartSchema.post('findOneAndUpdate', async function (doc) {
+  if (doc) {
+    doc.cartCountProducts = doc.cartProducts.reduce(
+      (arr: number, curr: CartProduct) => {
+        return arr + curr.quantity;
+      },
+      0,
+    );
+    await doc.save();
+  }
 });
 
 export default mongoose.model(DOCUMENT_NAMES.CART, cartSchema);
